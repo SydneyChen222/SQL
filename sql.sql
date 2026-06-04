272bbb09c4265d250c41d7ee758cc09c45f32ae3
Xinyi (Sydney) Chen <62270429+SydneyChen222@users.noreply.github.com>
2026-06-04 10:44:49 -0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,5 +1,5 @@
---1. Date Spine
-"""CONTRACTS
+"""1. Date Spine
+CONTRACTS
 - contract_id
 - customer_id  
 - contract_start_date
@@ -23,14 +23,97 @@ with month as
   ) 
   select month, 
   count(distinct( case when date_trunc('month',contract_start_date)<= month and (date_trunc('month',contract_end_date) >= month or contract_end_date is null) 
-  then contract_id else 0 end)) as number_of_active_contracts, 
+  then contract_id else end)) as number_of_active_contracts, 
   case when date_trunc('month',contract_start_date)= month and (date_trunc('month',contract_end_date) >= month or contract_end_date is null) 
-  then contract_id else 0 end)) as number_of_new_contracts, 
+  then contract_id else end)) as number_of_new_contracts, 
   case when date_trunc('month',contract_start_date)<= month and (date_trunc('month',contract_end_date) = month ) 
-  then contract_id else 0 end)) as number_of_churned_contracts 
+  then contract_id else end)) as number_of_churned_contracts 
   from month 
   left join contracts 
   on month >= date_trunc('month',contract_start_date) 
   and (month <= date_trunc('month',contract_end_date) or contract_end_date is null) 
   group by 1 
-  order by 1 aesc
+  order by 1 asc
+
+"""2. Retention 
+CONTRACTS
+- contract_id
+- customer_id
+- contract_start_date
+- contract_end_date (NULL if still active)
+  
+Question:
+Write a SQL query that shows cohort retention. 
+  For each signup month, show what amount of customers still had an active contract 1 month later, 2 months later, and 3 months later.
+Output:
+  SIGNUP_MONTH
+TOTAL_CUSTOMERS
+RETAINED_MONTH_1
+RETAINED_MONTH_2
+RETAINED_MONTH_3"""
+
+with first as (
+  select distinct(customer_id), min(date_trunc('month',contract_start_date)) as signup_month
+  from contracts
+  group by 1
+)
+  select signup_month,
+  count(distinct(customer_id)) as total_customers,
+count(distinct(
+  case when exists (
+  select 1 from contracts
+  where 
+  signup_month + interval '1 month' >= date_trunc('month', contract_start_date)
+  and (signup_month + interval '1 month' <= date_trunc('month', contract_end_date)
+  or contract_end_date is null)
+  ) then customer_id else end)
+) as retained_month_1,
+  
+  count(distinct(
+  case when (exists (
+  select 1 from contracts
+  where 
+  signup_month + interval '1 month' >= date_trunc('month', contract_start_date)
+  and (signup_month + interval '1 month' <= date_trunc('month', contract_end_date)
+  or contract_end_date is null)
+  ))
+  and (exists (
+  select 1 from contracts
+  where 
+  signup_month + interval '2 month' >= date_trunc('month', contract_start_date)
+  and (signup_month + interval '2 month' <= date_trunc('month', contract_end_date)
+  or contract_end_date is null)
+  ))
+  then customer_id else end)
+) as retained_month_2,
+
+  count(distinct(
+  case when (exists (
+  select 1 from contracts
+  where 
+  signup_month + interval '1 month' >= date_trunc('month', contract_start_date)
+  and (signup_month + interval '1 month' <= date_trunc('month', contract_end_date)
+  or contract_end_date is null)
+  ))
+  and (exists (
+  select 1 from contracts
+  where 
+  signup_month + interval '2 month' >= date_trunc('month', contract_start_date)
+  and (signup_month + interval '2 month' <= date_trunc('month', contract_end_date)
+  or contract_end_date is null)
+  ))
+  and (exists (
+  select 1 from contracts
+  where 
+  signup_month + interval '3 month' >= date_trunc('month', contract_start_date)
+  and (signup_month + interval '3 month' <= date_trunc('month', contract_end_date)
+  or contract_end_date is null)
+  ))
+  then customer_id else end)
+) as retained_month_3
+  
+from first
+group by 1
+order by 1
+
+  
