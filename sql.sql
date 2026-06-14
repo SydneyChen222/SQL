6248e3bc8408f579423929355e9831e82a297929
Xinyi (Sydney) Chen <62270429+SydneyChen222@users.noreply.github.com>
2026-06-14 16:53:03 -0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -772,5 +772,69 @@ left join numbered p
     on m.partner_rn = p.rn
 order by m.id;
 
+
+"""showing median for employee's salary
+  +--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| company      | varchar |
+| salary       | int     |
++--------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the company and the salary of one employee.
+ 
+
+Write a solution to find the rows that contain the median salary of each company. While calculating the median, when you sort the salaries of the company, break the ties by id.
+
+Return the result table in any order.
+
+The result format is in the following example.
+  Explanation: 
+For company A, the rows sorted are as follows:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 3  | A       | 15     |
+| 2  | A       | 341    |
+| 5  | A       | 451    | <-- median
+| 6  | A       | 513    | <-- median
+| 1  | A       | 2341   |
+| 4  | A       | 15314  |
++----+---------+--------+
+For company B, the rows sorted are as follows:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 8  | B       | 13     |
+| 7  | B       | 15     |
+| 12 | B       | 234    | <-- median
+| 11 | B       | 1221   | <-- median
+| 9  | B       | 1154   |
+| 10 | B       | 1345   |
++----+---------+--------+
+For company C, the rows sorted are as follows:
++----+---------+--------+
+| id | company | salary |
++----+---------+--------+
+| 17 | C       | 65     |
+| 13 | C       | 2345   |
+| 14 | C       | 2645   | <-- median
+| 15 | C       | 2645   | 
+| 16 | C       | 2652   |
++----+---------+--------+
+  """
+with cte as (
+    select 
+        id,
+        company,
+        salary,
+        row_number() over(partition by company order by salary, id) as rn,
+        count(*) over(partition by company) as cnt
+    from employee
+)
+select id, company, salary
+from cte
+where rn between (cnt + 1) / 2 and (cnt + 2) / 2;
   
 
